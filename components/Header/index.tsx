dbdaf98eb066e7b39f2bdc696a32179e04f05aa1
gutolive09 <gutolive01@gmail.com>
2024-03-06 19:17:12 -0300

    Add basic header styling.

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,14 +1,28 @@
-import { StyleSheet, Text, View, Platform, StatusBar } from 'react-native'
+import { StyleSheet, Text, View, Platform, StatusBar } from "react-native";
 
 export default function Header() {
   return (
     <View style={styles.header}>
-      <Text>Header</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>Header</Text>
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  header:{
-  paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
-},})
+  header: {
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+    width: "100%",
+    height: 100,
+    backgroundColor: "blue",
+  },
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 35,
+  },
+});
