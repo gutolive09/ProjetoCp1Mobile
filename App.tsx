44cbde6b73a6c483db17a4b58bdb8b9d92a97b91
gutolive09 <gutolive01@gmail.com>
2024-03-07 22:48:42 -0300

    Estrutura de Items criada e items preenchidos.
    Co-authored-by: Mateus Mantovani Araújo <Mmateus106@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import { ImageBackground, SafeAreaView, StyleSheet} from 'react-native';
+import { ImageBackground, SafeAreaView, ScrollView, StyleSheet} from 'react-native';
 import Header from "./components/Header"
 import Home from './components/Home';
 
 export default function App() {
   return (
+    
     <SafeAreaView style={styles.container}>
-      <ImageBackground source={require('./assets/img/escola.jpg')} resizeMode='cover'/>
       <Header/>
+      
+      <ImageBackground source={require('./assets/img/escola.jpg')} resizeMode='cover' style={styles.image}>
+      <ScrollView>
       <Home/>
+      </ScrollView>
+      </ImageBackground>
     </SafeAreaView>
   );
 }
@@ -16,11 +21,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     width: "100%",
+    height:"100%",
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
   image:{
-    flex:1,
-    justifyContent: "center"
+    height: "100%",
+    width: "100%",
   }
 });
